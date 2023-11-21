dd2aa278e3a6483c0cdd28ef8d65fc2c19d665a3
magihoefkens <magi.hoefkens@outlook.de>
2023-11-21 09:25:40 +0100

    modified the getProductByID so that returns an Optional, modified the Tests

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,7 +1,13 @@
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-
+import java.util.stream.Collectors;
+@Builder
+@Data
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
@@ -9,17 +15,28 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
+            Optional<Product> productToOrder = productRepo.getProductById(productId);
+            productToOrder.ifPresentOrElse(
+                    product -> products.add(product),
+                    ()->System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!"));
+            /*
             if (productToOrder == null) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
             }
-            products.add(productToOrder);
+            products.add(productToOrder);*/
         }
 
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products,OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
+    }
+    //returns a list of all orders with a specific order status (parameter) using streams
+    public List<Order> getAllOrdersWithOrderStatus(OrderStatus orderStatus){
+        List<Order> orders=this.orderRepo.getOrders();
+        return orders.stream().
+                filter(order -> order.orderStatus().equals(orderStatus))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -9,7 +9,16 @@ class ShopServiceTest {
     @Test
     void addOrderTest() {
         //GIVEN
-        ShopService shopService = new ShopService();
+        Product product=new Product("P1","Produkt1");
+        ProductRepo productRepo=new ProductRepo();
+        productRepo.addProduct(product);
+        Order newOrder1 = new Order("1", List.of(product),OrderStatus.PROCESSING);
+        OrderMapRepo repo=new OrderMapRepo();
+        repo.addOrder(newOrder1);
+        ShopService shopService = ShopService.builder()
+                .productRepo(productRepo)
+                .orderRepo(repo)
+                .build();
         List<String> productsIds = List.of("1");
 
         //WHEN
@@ -20,17 +29,50 @@ class ShopServiceTest {
         assertEquals(expected.products(), actual.products());
         assertNotNull(expected.id());
     }
-
     @Test
+    void getAllOrdersWhenOrderStatusPROCESSING(){
+        //GIVEN
+
+        Product product = new Product("1", "Apfel");
+        Order newOrder1 = new Order("1", List.of(product),OrderStatus.PROCESSING);
+        Order newOrder2 = new Order("2", List.of(product),OrderStatus.PROCESSING);
+        OrderMapRepo repo = new OrderMapRepo();
+        ProductRepo productRepo=new ProductRepo();
+        productRepo.addProduct(product);
+
+        repo.addOrder(newOrder1);
+        repo.addOrder(newOrder2);
+        ShopService shopService=ShopService.builder()
+                .productRepo(productRepo)
+                .orderRepo(repo)
+                .build();
+        //WHEN
+        List<Order>actual=shopService.getAllOrdersWithOrderStatus(OrderStatus.PROCESSING);
+        //THEN
+        List<Order> expected=List.of(newOrder1,newOrder2);
+        assertEquals(expected,actual);
+    }
+
+   /* @Test
     void addOrderTest_whenInvalidProductId_expectNull() {
         //GIVEN
-        ShopService shopService = new ShopService();
-        List<String> productsIds = List.of("1", "2");
+        //ShopService shopService = ShopService.builder().build();
+        Product product=new Product("P1","Produkt1");
+        ProductRepo productRepo=new ProductRepo();
+        productRepo.addProduct(product);
+        Order newOrder1 = new Order("1", List.of(product),OrderStatus.PROCESSING);
+        OrderMapRepo repo=new OrderMapRepo();
+        repo.addOrder(newOrder1);
+        ShopService shopService = ShopService.builder()
+                .productRepo(productRepo)
+                .orderRepo(repo)
+                .build();
+        List<String> productsIds = List.of("T1", "T2");
 
         //WHEN
         Order actual = shopService.addOrder(productsIds);
 
         //THEN
         assertNull(actual);
-    }
+    }*/
 }
